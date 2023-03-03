cb2f595f5b5ed0d9ed324a736081458b5c6363d5
Pranav Kumar <pranavkumar97954@gmail.com>
2023-03-03 18:58:24 +0530

    Category Finished

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("",views.home),
-]
+    path("category/<slug:val>/",views.CategoryView.as_view(),name="category"),
+    path("category-title/<val>",views.CategoryTitle.as_view(),name="category-title"),
+    path("product-detail/<int:pk>/",views.ProductDetail.as_view(),name="product-detail"),
+    path("about/",views.about,name="about"),
+    path("contact/",views.contact,name="contact"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.views import View
+from . models import Product
+from django.db.models import Count
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request,"app/category.html",locals())
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request,"app/productdetails.html",locals())
+    
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
