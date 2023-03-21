d62d3ff483929b690c712aa58e015aeaced9af38
Pranav Kumar <pranavkumar97954@gmail.com>
2023-03-21 16:30:59 +0530

    Fifth commit

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm
+from django.contrib.auth.models import User 
+from .models import Customer
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput({'autofocus':'True','class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput({'autocomplete':'current-password','class':'form-control'}))
+
+class CustomerRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput({'autofocus' : 'True','class':'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput({'class':'form-control'}))
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput({'class':'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput({'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+    
+class MyPasswordResetForm(PasswordChangeForm):
+    pass
+
+class CustomerProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name','mobile','locality','city','state','zipcode']
+        widgets={
+            'name':forms.TextInput({'class':'form-control'}),
+            'locality':forms.TextInput({'class':'form-control'}),
+            'city':forms.TextInput({'class':'form-control'}),
+            'mobile':forms.NumberInput({'class':'form-control'}),
+            'state':forms.Select({'class':'form-control'}),
+            'zipcode':forms.NumberInput({'class':'form-control'}),
+
+        }
--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from urllib import request
 from django.http import HttpResponse
 from django.views import View
-from . models import Product
+from . models import Product,Customer
 from django.db.models import Count
+from . forms import CustomerRegistrationForm,CustomerProfileForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -32,3 +34,50 @@ def about(request):
 
 def contact(request):
     return render(request,"app/contact.html")
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request,'app/customerregistration.html',locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Congratulation! User Register Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")        
+        return render(request,'app/customerregistration.html',locals())
+
+class ProfileView(View):
+    def get(self,request):
+        form = CustomerProfileForm()
+        return render(request,'app/profile.html',locals())
+    def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user,name=name,locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,"Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request,'app/profile.html',locals())
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request,'app/address.html',locals())
+
+class updateAddress(View):
+    def get(self,request,pk):
+        form = CustomerProfileForm()
+        return render(request,'app/updateAddress.html',locals())
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        return render(request,'app/updateAddress.html',locals())
