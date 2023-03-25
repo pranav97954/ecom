95a3fc773b814e84db78c7ff982dd00df75fb160
Pranav Kumar <93419620+pranav97954@users.noreply.github.com>
2023-03-26 02:13:11 +0530

    Update urls.py

--- ec/urls.py
+++ ec/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.conf.urls import url
-from django.views.static import serve
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("app.urls")),
-    #Hosting
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+   
 ]
